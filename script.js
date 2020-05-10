9ba30f9edaf447d103bdcd353f077c912ed0ce20
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-10 02:06:09 +0100

    code

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function () {
+    // Begin accessing JSON data here
+}
