ce6401f84cb276f8fff4af193a97a55357d9361b
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-10 02:13:21 +0100

    code

--- script.js
+++ script.js
@@ -21,3 +21,5 @@ const logo = document.createElement('img')
 logo.src = 'logo.png'
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
+app.appendChild(logo)
+app.appendChild(container)
